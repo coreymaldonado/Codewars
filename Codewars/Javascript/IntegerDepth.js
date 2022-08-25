e2e8c2dfe1bacd032d01a271a375460d69e0c4bf
UntoldDawn <coreyam3@gmail.com>
2022-08-24 21:11:39 -0400

    Codewars 08/24/22

---
+++ Codewars/Javascript/IntegerDepth.js
@@ -0,0 +1,37 @@
+// The depth of an integer n is defined to be how many multiples of n it is necessary to compute before all 10 digits have appeared at least once in some multiple.
+
+// example:
+
+// let see n=42
+
+// Multiple         value         digits     comment
+// 42*1              42            2,4 
+// 42*2              84             8         4 existed
+// 42*3              126           1,6        2 existed
+// 42*4              168            -         all existed
+// 42*5              210            0         2,1 existed
+// 42*6              252            5         2 existed
+// 42*7              294            9         2,4 existed
+// 42*8              336            3         6 existed 
+// 42*9              378            7         3,8 existed
+// Looking at the above table under digits column you can find all the digits from 0 to 9, Hence it required 9 multiples of 42 to get all the digits. So the depth of 42 is 9. Write a function named computeDepth which computes the depth of its integer argument.Only positive numbers greater than zero will be passed as an input.
+
+function computeDepth (x){
+    let digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    let digitsFound = []
+    let currentSplit
+    let count = 1
+    for ( let h = 1; h != 0; h++){
+      currentSplit = String(x * count).split('')
+      for ( let i = 0; i < currentSplit.length; i++){
+        if ( !digitsFound.includes(currentSplit[i]) ){
+          digitsFound.push(currentSplit[i])
+        }      
+      }
+      if ( digitsFound.length == digits.length ) {
+        h = -1
+      }
+      count++
+    }
+    return count - 1;
+  }
