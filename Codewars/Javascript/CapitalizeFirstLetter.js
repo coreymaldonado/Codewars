826494ae15f45e265cb4d6d0b6b7ed46ba8b6578
UntoldDawn <coreyam3@gmail.com>
2022-08-26 14:12:37 -0400

    08/26/2022

---
+++ Codewars/Javascript/CapitalizeFirstLetter.js
@@ -0,0 +1,26 @@
+// Capitalize First Letter of a String
+// Write a function capitalize() which capitalizes the first letter (if any) of the given string. For example:
+
+// Input	Output
+// string	String
+// hello World	Hello World
+// i love codewars	I love codewars
+// This sentence is already capitalized	This sentence is already capitalized
+// 0123the first character of this sentence is not a letter	0123the first character of this sentence is not a letter
+// JavaScript / CoffeeScript: Extend the String prototype with a method capitalize() so you can call it on a string like so: "string".capitalize(). Learn about inheritance and the prototype chain.
+
+// Furthermore, the built-in string methods toUpperCase() and toLowerCase() are disabled for this Kata.
+
+String.prototype.capitalize = function(string) {
+    var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    var lettersCap = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+    var str = this.split('')
+    if ( letters.includes(str[0]) ){
+      for (let i = 0; i < letters.length; i++){
+        if (letters[i] == str[0]){
+          str[0] = lettersCap[i];
+        }
+      }
+    }
+    return str.join('')
+  };
